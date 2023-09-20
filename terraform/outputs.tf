469d5ba35dffe37ce8379b042616ebedbf805b09
Hazel Yap <hazel.yap@samsung.com>
2023-09-20 10:38:57 +0800

    Initial commit for Ansible files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,18 +1,18 @@
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
+#output "vpc_id" {
+#  value = module.vpc.vpc_id
+#}
 
-output "public_subnet_ids" {
-  value = module.vpc.public_subnet_ids
-}
+#output "public_subnet_ids" {
+#  value = module.vpc.public_subnet_ids
+#}
 
-output "private_subnet_ids" {
-  value = module.vpc.private_subnet_ids
-}
+#output "private_subnet_ids" {
+#  value = module.vpc.private_subnet_ids
+#}
 
-output "vpc_endpoint_id" {
-  value = module.vpc.vpc_endpoint_id
-}
+#output "vpc_endpoint_id" {
+#  value = module.vpc.vpc_endpoint_id
+#}
 
 output "alb_sg_id" {
   value = module.alb.alb_sg_id
